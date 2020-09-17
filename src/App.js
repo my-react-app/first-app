30413cb116599ca5daec31721a9bf318460bc2f6
Eugene S <react.10.09.2020@gmail.com>
2020-09-17 22:38:57 +0300

    changed components to class

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="wrapper">
-      <header className="header">
-        <span>header</span>
-      </header>
-      <main className="main">
-        <aside className="aside">
-          <span>aside</span>
-        </aside>
-        <div className="content">
-          <span>content</span>
-        </div>
-      </main>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <>
+        <header className="header">
+          <span>header</span>
+        </header>
+        <main className="main">
+          <aside className="aside">
+            <span>aside</span>
+          </aside>
+          <div className="content">
+            <span>content</span>
+          </div>
+        </main>
+      </>
+    )
+  }
 }
-
-export default App;
